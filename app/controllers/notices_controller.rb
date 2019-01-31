e143382fdaa07aa1950f50ef539dc04a68bce983
Le Tat Tung <letattungtb@gmail.com>
2019-01-31 19:03:03 +0900

    build layout

---
+++ app/controllers/notices_controller.rb
@@ -0,0 +1,16 @@
+class NoticesController < ApplicationController
+
+  def create
+    @notice = Notice.new(notice_params)
+    respond_to do |format|
+      if @notice.save
+        format.html { redirect_to root_path }
+      end
+    end
+  end
+
+  private
+  def notice_params
+    params.require(:notice).permit(:body, :sender_id, :recipient_id)
+  end
+end
